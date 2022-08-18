ce1c73049aca3b815ffe6b9c9043284bfd4cb3dc
mshornikov <mikhail.shornikov@mail.ru>
2022-08-18 13:47:12 +0300

    Add links to themes

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('browserSync', function() {
 // For recompilation
 gulp.task('watch', function() {
     gulp.watch('src/**/*.scss', gulp.series('default')).on('change', reload),
-    gulp.watch('src/*.html', gulp.series('fileinclude')).on('change', reload);
+    gulp.watch('src/*.html', gulp.series('htmlCopy')).on('change', reload);
     gulp.watch('./src/img/**', gulp.series("imageCopy")).on("change", reload);
 });
 
