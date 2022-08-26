daba0033d835f3b496d6b7efd128d8f2de5c7213
saintty <aauzinger@yandex.ru>
2022-08-26 18:57:13 +0300

    empty

--- build/js/index.js
+++ build/js/index.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    const mediaQuery = window.matchMedia('(max-width: 1100px)');
+    const mediaQuery = window.matchMedia('(max-width: 1145px)');
     if (mediaQuery.matches) transformColumnView(mediaQuery);
     mediaQuery.addListener(transformColumnView);
 
