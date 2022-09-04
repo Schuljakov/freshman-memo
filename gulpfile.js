f23c5066887c0f2c2d9ebf5bb0558413ce44505f
saintty <aauzinger@yandex.ru>
2022-09-04 09:30:33 +0300

    added .edictorconfig for vs code, changed scss-rules file for scss format, changed all 'require' requesrs to 'import' requests

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,45 @@
-const { src, dest, watch, parallel, series} = require('gulp');
-const fs = require('fs');
+import gulp from 'gulp';
+import src from 'gulp';
+import dest from 'gulp';
+import fs from 'fs';
 
-const pug          = require('gulp-pug');
-const autoprefixer = require('gulp-autoprefixer');
-const scss         = require('gulp-sass')(require('sass'));
-const uglify       = require('gulp-uglify-es').default;
-const imagemin     = require('gulp-imagemin');
+import pug from 'gulp-pug';
+import scss from 'gulp-sass';
+import csso from 'gulp-csso';
+import autoprefixer from 'gulp-autoprefixer';
+import uglify from 'gulp-uglify-es';
+import imagemin  from 'gulp-imagemin';
+import concat from 'gulp-concat';
+import del from 'del';
+import cleanCss from 'gulp-clean-css';
+import plumber from 'gulp-plumber';
+import gcmq from 'gulp-group-css-media-queries';
+import browserSync from 'browser-sync';
+import path from 'path';
 
-const concat       = require('gulp-concat');
-const del          = require('del');
-const cleanCss     = require('gulp-clean-css');
-const plumber      = require('gulp-plumber');
-const gcmq         = require('gulp-group-css-media-queries');
+const data = {};
 
-const browserSync  = require('browser-sync').create();
-const path         = require('path');
-const data         = {};
+// const { src, dest, watch, parallel, series} = require('gulp');
+// const fs = require('fs');
 
-function json() {
+// const pug          = require('gulp-pug');
+// const scss         = require('gulp-sass')(require('sass'));
+// const csso         = require('gulp-csso');
+// const autoprefixer = require('gulp-autoprefixer');
+// const uglify       = require('gulp-uglify-es').default;
+// const imagemin     = require('gulp-imagemin');
+
+// const concat       = require('gulp-concat');
+// const del          = require('del');
+// const cleanCss     = require('gulp-clean-css');
+// const plumber      = require('gulp-plumber');
+// const gcmq         = require('gulp-group-css-media-queries');
+
+// const browserSync  = require('browser-sync').create();
+// const path         = require('path');
+// const data         = {};
+
+export const json = () => {
     try {
         const modules = fs.readdirSync('src/data/');
         modules.forEach(json => {
@@ -25,13 +47,12 @@ function json() {
             const file = path.join('./src/data', json);
             return data[name] = JSON.parse(fs.readFileSync(file));
         })
-        console.log(data);
     } catch(e) {
         console.log(e);
     }
 }
 
-function pug2html() {
+export const pug2html = () => {
     return src('src/pages/*.pug')
         .pipe(plumber())
         .pipe(pug({
@@ -42,20 +63,20 @@ function pug2html() {
         .pipe(browserSync.stream());
 }
 
-function collectComponentsSCSS() {
+export const collectComponentsSCSS = () => {
     return src('src/components/**/*.scss')
         .pipe(concat('_components.scss'))
         .pipe(dest('src/components'));
 }
 
-function collectComponentMediaSCSS() {
+export const collectComponentMediaSCSS = () => {
     return src('src/components/**/media.scss')
         .pipe(gcmq())
         .pipe(concat('_media.scss'))
         .pipe(dest('src/components'));
 }
 
-function styles() {
+export const styles = () => {
     return src('src/scss/style.scss')
         .pipe(scss({outputStyle: 'compressed'}))
         .pipe(concat('style.min.css'))
@@ -63,12 +84,13 @@ function styles() {
             overrideBrowserslist: ['last 12 version'],
             grid: true
         }))
+        .pipe(csso())
         .pipe(cleanCss({compatibility: 'ie8', level: 2}))
         .pipe(dest('build/css'))
         .pipe(browserSync.stream());
 }
 
-function scripts() {
+export const scripts = () => {
     return src([
         'src/components/**/*.js',
         'src/js/main.js'
@@ -79,15 +101,22 @@ function scripts() {
     .pipe(browserSync.stream());
 }
 
-function browsersync() {
-    browserSync.init({
+export const browsersync = () => {
+    browserSync.create().init({
         server: {
             baseDir: 'build/'
-        }
+        },
+        files: [
+          "build/css/*.css",
+          "build/js/*.js",
+          "build/*.html"
+        ],
+        open: false,
+        notify: false,
     });
 }
 
-function imagesBuild() {
+export const imageBuild = () => {
     return src(['src/images/**/*', 'src/components/**/*'])
         .pipe(imagemin([
             imagemin.gifsicle({interlaced: true}),
@@ -111,29 +140,29 @@ function imagesBuild() {
 //         .pipe(dest("build/images"));
 // }
 
-function watching() {
-    watch(['src/pages/*.pug'], pug2html);
-    watch('src/data/*.json', json, pug2html);
-    watch(['src/components/**/*.scss', '!src/components/**/media.scss'], collectComponentsSCSS, styles);
-    watch(['src/components/**/media.scss'], collectComponentMediaSCSS, styles);
-    watch(['src/scss/**/*.scss'], styles);
-    watch(['src/components/**/*.js', 'src/js/main.js'], scripts);
+export const watching = () => {
+    gulp.watch(['src/pages/*.pug'], pug2html);
+    gulp.watch('src/data/*.json', json, pug2html);
+    gulp.watch(['src/components/**/*.scss', '!src/components/**/media.scss'], collectComponentsSCSS, styles);
+    gulp.watch(['src/components/**/media.scss'], collectComponentMediaSCSS, styles);
+    gulp.watch(['src/scss/**/*.scss'], styles);
+    gulp.watch(['src/components/**/*.js', 'src/js/main.js'], scripts);
 }
 
-function cleanBuild() {
-    return del('build');
-}
 
-exports.pug2html = pug2html;
-exports.json = json;
-exports.styles = styles;
-exports.collectComponentsSCSS = collectComponentsSCSS;
-exports.collectComponentsMediaSCSS = collectComponentMediaSCSS;
-exports.browsersync = browsersync;
-exports.watching = watching;
-exports.scripts = scripts;
-exports.del = cleanBuild;
+export const startGulp = gulp.series(json, pug2html, collectComponentsSCSS, collectComponentMediaSCSS, styles, scripts);
+export const buildImages = gulp.series(imagemin);
+export const run = gulp.parallel(json, browsersync, watching);
+// exports.pug2html = pug2html;
+// exports.json = json;
+// exports.styles = styles;
+// exports.collectComponentsSCSS = collectComponentsSCSS;
+// exports.collectComponentsMediaSCSS = collectComponentMediaSCSS;
+// exports.browsersync = browsersync;
+// exports.watching = watching;
+// exports.scripts = scripts;
+// exports.del = cleanBuild;
 
-exports.startGulp = series(pug2html, collectComponentsSCSS, collectComponentMediaSCSS, styles, scripts);
-exports.buildImages = series(imagemin);
-exports.default = parallel(json, browsersync, watching);
+// exports.startGulp = series(pug2html, collectComponentsSCSS, collectComponentMediaSCSS, styles, scripts);
+// exports.buildImages = series(imagemin);
+// exports.default = parallel(json, browsersync, watching);
